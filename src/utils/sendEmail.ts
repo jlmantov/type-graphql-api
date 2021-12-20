f8d44ffb45eebd4e1c9a15394e41cfc2bf48fe9a
Janick Mantov <janick.mantov@gmail.com>
2021-12-20 19:25:07 +0100

    added: resetPasswordEmail

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -3,8 +3,11 @@ import nodemailer from "nodemailer";
 import { v4 } from "uuid";
 import { User } from "../entity/User";
 import { UserEmail } from "../entity/UserEmail";
+import { resetPasswordToken } from "./auth";
+import { resetPasswordForm } from "./resetPasswordForm";
 
 export const CONFIRMUSER = "confirm";
+export const RESETPWD = "resetpwd";
 
 /**
  * Verify uuid from request url, enable login and cleanup email confirmation
@@ -40,9 +43,42 @@ export const confirmUserEmail = async (req: Request, res: Response) => {
 };
 
 /**
+ * 1. Verify uuid from request url,
+ * 2. create accessToken to changePassword
+ * 3. cleanup email
+ * 4. redirect to change password
+ * @param http request - url parameter is used as input
+ * @param http status - 200 on success, 400 on error
+ * @param http response - succes JSON: { OK: true } - error JSON: { error: 'message' }
+ * @returns http response - true when user is confirmed, false otherwise
+ */
+export const resetPasswordEmail = async (req: Request, res: Response) => {
+  console.log("req.params", JSON.stringify(req.params));
+
+  // request endpoint is "/user/confirm/:id" - this means that req.params.id is defined when this method is called
+  const userReset = await UserEmail.findOne({ where: { uuid: req.params.id, reason: RESETPWD } });
+  // console.log("resetPasswordEmail", userReset);
+  if (userReset === undefined) {
+    res.status(400).json({ error: "Expired or unknown id, please reset again!" });
+  } else {
+    const user = await User.findOne({ where: { email: userReset.email } });
+    console.log("user", user);
+    if (!user) {
+      res.status(400).json({ error: "Not found!" });
+    } else {
+      // now create a JSON Web Token - https://www.npmjs.com/package/jsonwebtoken
+      const resetPwdToken = await resetPasswordToken(user);
+      res.set({ Authorization: "bearer " + resetPwdToken });
+      const html = resetPasswordForm(resetPwdToken);
+      res.send(html);
+    }
+  }
+};
+
+/**
  * Use nodemailer to create an email with a unique URL
  * 1. let user enable login
- * 2. TO DO: let user reset password
+ * 2. let user reset password
  * @param email address to send confirmation email to, before enabling user login
  */
 export const sendUserEmail = async (email: string, reason: string) => {
@@ -73,6 +109,11 @@ export const sendUserEmail = async (email: string, reason: string) => {
   };
 
   switch (reason) {
+    case RESETPWD:
+      mailOptions.subject = "Reset Password"; // Subject line
+      mailOptions.text = `Please click this link in order to reset your password: ${url}`; // plain text body
+      mailOptions.html = `<b>Please click this link in order to reset your password.</b><a href="${url}">${url}</a>`; // html body
+      break;
     case CONFIRMUSER:
     default:
       mailOptions.subject = "Confirm Email"; // Subject line
