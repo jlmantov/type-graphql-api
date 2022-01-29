84864c26b6d5fd560a5bf34132ddd138c0c06722
Janick Mantov <janick.mantov@gmail.com>
2022-01-29 18:05:32 +0100

    TypeORM: migration beautify

--- src/orm/migration/1642104804801-UserEmails.ts
+++ src/orm/migration/1642104804801-UserEmails.ts
@@ -1,15 +1,24 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class UserEmails1642104804801 implements MigrationInterface {
-    name = 'UserEmails1642104804801'
+  name = "UserEmails1642104804801";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`UserEmails\` (\`id\` int NOT NULL AUTO_INCREMENT, \`email\` varchar(255) NOT NULL, \`uuid\` varchar(36) NOT NULL, \`reason\` varchar(255) NOT NULL, \`createdAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, UNIQUE INDEX \`IDX_713b28096da96c815cc475a190\` (\`uuid\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      "CREATE TABLE `UserEmails` (" +
+        "`id` int NOT NULL AUTO_INCREMENT, " +
+        "`email` varchar(255) NOT NULL, " +
+        "`uuid` varchar(36) NOT NULL, " +
+        "`reason` varchar(255) NOT NULL, " +
+        "`createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+        "UNIQUE INDEX `IDX_713b28096da96c815cc475a190` (`uuid`), " +
+        "PRIMARY KEY (`id`)) " +
+        "ENGINE=InnoDB"
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`IDX_713b28096da96c815cc475a190\` ON \`UserEmails\``);
-        await queryRunner.query(`DROP TABLE \`UserEmails\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX \`IDX_713b28096da96c815cc475a190\` ON \`UserEmails\``);
+    await queryRunner.query(`DROP TABLE \`UserEmails\``);
+  }
 }
