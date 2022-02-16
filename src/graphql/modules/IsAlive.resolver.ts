019468e33f3fc245cbe82f2f789c8c78ef7a43c7
Janick Mantov <janick.mantov@gmail.com>
2022-02-16 18:52:50 +0100

    fixed IsAliveResolver error: 'TypeError: Reflect.getMetadata is not a function'

--- src/graphql/modules/IsAlive.resolver.ts
+++ src/graphql/modules/IsAlive.resolver.ts
@@ -1,36 +1,15 @@
+import "reflect-metadata";
 import { Query, Resolver } from "type-graphql";
-import logger from "../../utils/middleware/winstonLogger";
 
 /**
  * IsAlive returns a UTC timestamp
- * https://typegraphql.com/docs/extensions.html#using-the-extensions-decorator
+ * https://typegraphql.com/docs/resolvers.html#resolver-classes
  */
-// @ObjectType()
-// class IsAliveResponse {
-//   @Field()
-//   utcTimestamp: number;
-// }
-
 @Resolver()
 export class IsAliveResolver {
-  @Query(() => Number) // Tell type-graphql that return value is of type IsAliveResponse
+  @Query(() => Number) // Tell type-graphql that return value is of type Number
   async isAlive() {
-    // tell TypeScript that IsAlive returns a promise of type IsAliveResponse
-
-    // create a JSON Web Token - https://www.npmjs.com/package/jsonwebtoken
     const utcTimestamp = new Date().getTime();
-    logger.silly(" -- IsAlive --> utcTimestamp: ", { utcTimestamp });
     return utcTimestamp;
   }
-
-  // async isAlive(): Promise<IsAliveResponse> {
-  //   // tell TypeScript that IsAlive returns a promise of type IsAliveResponse
-
-  //   // create a JSON Web Token - https://www.npmjs.com/package/jsonwebtoken
-  //   const utcTimestamp = new Date().getTime();
-  //   logger.silly(" -- IsAlive --> utcTimestamp: ", { utcTimestamp });
-  //   return {
-  //     utcTimestamp,
-  //   };
-  // }
 }
